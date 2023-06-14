621c4dec83f940e8511b63ee98620e97c4784577
amfnet <ang_fersar@outlook.es>
2023-06-14 18:03:11 -0400

    register view

--- src/router/router.index.tsx
+++ src/router/router.index.tsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import TodoView from "../pages/TodoPage/TodoView";
 import ErrorView from "../pages/ErroPage/ErrorView";
 import LoginView from "../pages/LoginPage/LoginView";
+import RegisterView from "../pages/RegisterPage/RegisterView";
 
 
 export const router = createBrowserRouter([
@@ -13,5 +14,9 @@ export const router = createBrowserRouter([
    {
       path: "/login",
       element: <LoginView />
+   },
+   {
+      path: "/register",
+      element: <RegisterView />
    }
 ])
